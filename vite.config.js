1d1c905044478ce15bc24b8cd9b7e9724eaf0b69
Nitesh Bhardwaj <167672274+nkbcatheboy2@users.noreply.github.com>
2026-05-30 10:56:31 +0530

    Refactor Vite config to remove Tailwind CSS
    Removed tailwindcss import and adjusted plugins.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- 1. Isko import karo
+  import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <-- 2. Isko plugins ke andar daal do
-  ],
-})
+  export default defineConfig({
+    plugins: [react()],
+    base: './', // 
+  })

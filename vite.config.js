1b07dba58116564ca91b0f5447964c40c36d1f00
Nitesh Bhardwaj <167672274+nkbcatheboy2@users.noreply.github.com>
2026-06-02 14:39:40 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- 1. Isko import karo
 
-  export default defineConfig({
-    plugins: [react()],
-    base: './', // 
-  })
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // <-- 2. Isko plugins ke andar daal do
+  ],
+})
